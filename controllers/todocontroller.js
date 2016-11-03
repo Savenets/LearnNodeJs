e6d70361755e8bc8690444da742a95a4ea39f5fb
nsaven <nickolay.savenets@stuzo.com>
2016-11-03 11:10:38 +0200

    e

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -1,17 +1,26 @@
 /**
  * Created by Nick on 11/2/2016.
  */
+var bodyParser = require('body-parser');
+
+var data = [{item: 'learn es6'}, {item: 'learn Angular 2'}, {item: 'learn reactjs'}];
+var urlencodedParser = bodyParser.urlencoded({extend:false});
+
 module.exports = function(app){
 
     app.get('/todo', function(req, res){
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
-    app.post('/todo', function(req, res){
-
+    app.post('/todo', urlencodedParser,function(req, res){
+       data.push(req.body);
+        res.json(data);
     });
 
     app.delete('/todo', function(req, res){
-
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     })
 
 
